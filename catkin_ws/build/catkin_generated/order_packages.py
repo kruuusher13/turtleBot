f0badd192bfce21407291d3af0ffebd92ab9afda
Romir <romir.malik13@gmail.com>
2023-09-05 17:43:09 +0530

    turtlebot sim

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/mnt/d/turtleBot/turtleBot/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/mnt/d/turtleBot/turtleBot/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/mnt/d/turtleBot/turtleBot/catkin_ws/devel;/opt/ros/noetic' != '' else []
